cd142fc0e7a99e8b6fced2a09cfcf3b0dd69bb26
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2024-02-09 18:38:33 +0100

    Add root php files to php cs fixer

--- rector.php
+++ rector.php
@@ -3,9 +3,8 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the CFONB Parser package.
+ * This file is part of the ZUGFeRD PHP package.
  *
- * (c) SILARHI <dev@silarhi.fr>
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
